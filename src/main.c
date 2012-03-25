eaca4cde4dd08e1c7d78ad03dcb8b22dbc2c48bb
Béchu Jérôme <bechu.jerome@gmail.com>
2012-03-25 22:27:53 +0200

    ca compile

--- src/main.c
+++ src/main.c
@@ -1,7 +1,24 @@
+#include "sys/axon2.h"
+#include "core.h"
+#include "timer.h"
+#include "uart.h"
+#include "rprintf.h"
 
-
-int main(void)
+void appInitHardware(void)
 {
-    return 0;
+    uartInit(UART1, 19200);
+    rprintfInit(&uart1SendByte);
 }
+
+TICK_COUNT appInitSoftware(TICK_COUNT loopStart)
+{
+    return 0; // dont pause after
+}
+
+TICK_COUNT appControl(LOOP_COUNT loopCount, TICK_COUNT loopStart)
+{
+    rprintf("Hello world\n");
+    return 1000000; // wait for 1 second before calling me again. 1000000us = 1
+}
+
 
