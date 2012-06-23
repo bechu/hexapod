d3d022c710b233c00ab7b7d5a725cd2cbe21bf9e
Jonathan Grizou <jonathan.grizou@gmail.com>
2012-06-23 14:50:01 -0700

    change in makefile to link to the library in the project

--- src_win/dep/beta.o.d
+++ src_win/dep/beta.o.d
@@ -1,138 +1,125 @@
-beta.o: beta.cpp hardware.h ../../Users/jerome/Documents/2/2.08/libdefs.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
-  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom640.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomxx0_1.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
-  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
-  ../../Users/jerome/Documents/2/2.08/core.h \
-  ../../Users/jerome/Documents/2/2.08/libdefs.h \
-  ../../Users/jerome/Documents/2/2.08/clock.h \
-  ../../Users/jerome/Documents/2/2.08/timer.h \
-  ../../Users/jerome/Documents/2/2.08/errors.h \
-  ../../Users/jerome/Documents/2/2.08/core.h \
-  ../../Users/jerome/Documents/2/2.08/a2d.h \
-  ../../Users/jerome/Documents/2/2.08/rprintf.h \
-  ../../Users/jerome/Documents/2/2.08/led.h \
-  ../../Users/jerome/Documents/2/2.08/iopin.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h \
-  ../../Users/jerome/Documents/2/2.08/switch.h \
-  ../../Users/jerome/Documents/2/2.08/errors.h \
-  ../../Users/jerome/Documents/2/2.08/uart.h \
-  ../../Users/jerome/Documents/2/2.08/_uart_common.h \
-  ../../Users/jerome/Documents/2/2.08/buffer.h \
-  ../../Users/jerome/Documents/2/2.08/Stream/Stream.h \
-  ../../Users/jerome/Documents/2/2.08/Stream/../libdefs.h \
-  ../../Users/jerome/Documents/2/2.08/Stream/../Sensors/_sensor_common.h \
-  ../../Users/jerome/Documents/2/2.08/Stream/../Sensors/../libdefs.h \
-  ../../Users/jerome/Documents/2/2.08/segled.h \
-  ../../Users/jerome/Documents/2/2.08/servos.h \
-  ../../Users/jerome/Documents/2/2.08/actuators.h \
-  ../../Users/jerome/Documents/2/2.08/uart.h \
-  ../../Users/jerome/Documents/2/2.08/i2cBus.h \
-  ../../Users/jerome/Documents/2/2.08/_i2c_common.h lib/lib_timerdef.h \
-  lib/lib_iopins.h lib/lib_ctor.h protocol.h smart_motor.h
+beta.o: beta.cpp hardware.h ../WebbotLibV2/libdefs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom640.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomxx0_1.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdio.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stdarg.h \
+  ../WebbotLibV2/core.h ../WebbotLibV2/libdefs.h ../WebbotLibV2/clock.h \
+  ../WebbotLibV2/timer.h ../WebbotLibV2/errors.h ../WebbotLibV2/core.h \
+  ../WebbotLibV2/a2d.h ../WebbotLibV2/rprintf.h ../WebbotLibV2/led.h \
+  ../WebbotLibV2/iopin.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdlib.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/eeprom.h \
+  ../WebbotLibV2/switch.h ../WebbotLibV2/errors.h ../WebbotLibV2/uart.h \
+  ../WebbotLibV2/_uart_common.h ../WebbotLibV2/buffer.h \
+  ../WebbotLibV2/Stream/Stream.h ../WebbotLibV2/Stream/../libdefs.h \
+  ../WebbotLibV2/Stream/../Sensors/_sensor_common.h \
+  ../WebbotLibV2/Stream/../Sensors/../libdefs.h ../WebbotLibV2/segled.h \
+  ../WebbotLibV2/servos.h ../WebbotLibV2/actuators.h \
+  ../WebbotLibV2/uart.h ../WebbotLibV2/i2cBus.h \
+  ../WebbotLibV2/_i2c_common.h lib/lib_timerdef.h lib/lib_iopins.h \
+  lib/lib_ctor.h protocol.h smart_motor.h
 
 hardware.h:
 
-../../Users/jerome/Documents/2/2.08/libdefs.h:
+../WebbotLibV2/libdefs.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h:
 
-c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom640.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom640.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomxx0_1.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomxx0_1.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/stdio.h:
 
-c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stdarg.h:
 
-../../Users/jerome/Documents/2/2.08/core.h:
+../WebbotLibV2/core.h:
 
-../../Users/jerome/Documents/2/2.08/libdefs.h:
+../WebbotLibV2/libdefs.h:
 
-../../Users/jerome/Documents/2/2.08/clock.h:
+../WebbotLibV2/clock.h:
 
-../../Users/jerome/Documents/2/2.08/timer.h:
+../WebbotLibV2/timer.h:
 
-../../Users/jerome/Documents/2/2.08/errors.h:
+../WebbotLibV2/errors.h:
 
-../../Users/jerome/Documents/2/2.08/core.h:
+../WebbotLibV2/core.h:
 
-../../Users/jerome/Documents/2/2.08/a2d.h:
+../WebbotLibV2/a2d.h:
 
-../../Users/jerome/Documents/2/2.08/rprintf.h:
+../WebbotLibV2/rprintf.h:
 
-../../Users/jerome/Documents/2/2.08/led.h:
+../WebbotLibV2/led.h:
 
-../../Users/jerome/Documents/2/2.08/iopin.h:
+../WebbotLibV2/iopin.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/stdlib.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h:
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/eeprom.h:
 
-../../Users/jerome/Documents/2/2.08/switch.h:
+../WebbotLibV2/switch.h:
 
-../../Users/jerome/Documents/2/2.08/errors.h:
+../WebbotLibV2/errors.h:
 
-../../Users/jerome/Documents/2/2.08/uart.h:
+../WebbotLibV2/uart.h:
 
-../../Users/jerome/Documents/2/2.08/_uart_common.h:
+../WebbotLibV2/_uart_common.h:
 
-../../Users/jerome/Documents/2/2.08/buffer.h:
+../WebbotLibV2/buffer.h:
 
-../../Users/jerome/Documents/2/2.08/Stream/Stream.h:
+../WebbotLibV2/Stream/Stream.h:
 
-../../Users/jerome/Documents/2/2.08/Stream/../libdefs.h:
+../WebbotLibV2/Stream/../libdefs.h:
 
-../../Users/jerome/Documents/2/2.08/Stream/../Sensors/_sensor_common.h:
+../WebbotLibV2/Stream/../Sensors/_sensor_common.h:
 
-../../Users/jerome/Documents/2/2.08/Stream/../Sensors/../libdefs.h:
+../WebbotLibV2/Stream/../Sensors/../libdefs.h:
 
-../../Users/jerome/Documents/2/2.08/segled.h:
+../WebbotLibV2/segled.h:
 
-../../Users/jerome/Documents/2/2.08/servos.h:
+../WebbotLibV2/servos.h:
 
-../../Users/jerome/Documents/2/2.08/actuators.h:
+../WebbotLibV2/actuators.h:
 
-../../Users/jerome/Documents/2/2.08/uart.h:
+../WebbotLibV2/uart.h:
 
-../../Users/jerome/Documents/2/2.08/i2cBus.h:
+../WebbotLibV2/i2cBus.h:
 
-../../Users/jerome/Documents/2/2.08/_i2c_common.h:
+../WebbotLibV2/_i2c_common.h:
 
 lib/lib_timerdef.h:
 
